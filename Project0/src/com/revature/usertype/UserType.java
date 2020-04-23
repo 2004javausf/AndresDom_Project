4b5050cbdb67582a26fdb1282279794212e95ae8
Dominique McBride <djbride@yahoo.com>
2020-04-23 14:20:34 -0400

    Pushed starting files

---
+++ Project0/src/com/revature/usertype/UserType.java
@@ -0,0 +1,18 @@
+package com.revature.usertype;
+
+import java.util.Scanner;
+
+public class UserType {
+	public static void main(String [] args) {
+	
+	//int optionClient=0;
+	//public void UserInput(){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("What kind of User are you?  1) Existing User 2) New User");
+		int selection = sc.nextInt();
+		
+		System.out.println("selection");
+		//}
+	}
+}
